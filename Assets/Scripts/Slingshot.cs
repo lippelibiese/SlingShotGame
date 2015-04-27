56d35aa946a0493a11f4bfe676bec3cf29779043
Philipp Liese <philipp.liese@stud.h-da.de>
2015-04-27 13:23:14 +0200

    slingshot made interactive -not shooting-

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+
+	private  GameObject halo;
+
+void Awake(){
+		Transform haloTrans = transform.Find ("launchpoint");
+		halo = haloTrans.gameObject;
+			halo.SetActive(false);
+
+}
+
+void OnMouseEnter(){
+		//print ("Slingshot:MouseEnter");
+		halo.SetActive (true);
+	}
+
+void OnMouseExit() {
+		//print ("Slingshot:MouseExit");
+		halo.SetActive (false);
+	}
+
+}
